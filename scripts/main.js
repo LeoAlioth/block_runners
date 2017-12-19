eee8f0b933e21a31a0ed662696b3831aa0ca73af
LeoAlioth <anzek.kosir@gmail.com>
2017-12-19 20:08:48 +0100

    a rotating cube is now moving

--- scripts/main.js
+++ scripts/main.js
@@ -247,6 +247,7 @@ function initBuffers() {
         [1.0, 0.0, 0.0, 1.0], // Front face
         [1.0, 1.0, 0.0, 1.0], // Back face
         [0.0, 1.0, 0.0, 1.0], // Top face
+
         [1.0, 0.5, 0.5, 1.0], // Bottom face
         [1.0, 0.0, 1.0, 1.0], // Right face
         [0.0, 0.0, 1.0, 1.0]  // Left face

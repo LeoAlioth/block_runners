a9c78e0f769e2795eb6b9d349c5ecc51c83ec8dc
klopiiga <33962754+klopiiga@users.noreply.github.com>
2018-01-02 14:33:57 +0100

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -151,66 +151,66 @@ function getShader(gl, id) {
 //
 // Initialize the shaders, so WebGL knows how to light our scene.
 //
-function initShaders(){
+function initShaders() {
     var fragmentShader = getShader(gl, "per-fragment-lighting-fs");
     var vertexShader = getShader(gl, "per-fragment-lighting-vs");
+  
+  // Create the shader program
+  shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+  
+  // If creating the shader program failed, alert
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    alert("Unable to initialize the shader program.");
+  }
 
-    // Create the shader program
-    shaderProgram = gl.createProgram();
-    gl.attachShader(shaderProgram, vertexShader);
-    gl.attachShader(shaderProgram, fragmentShader);
-    gl.linkProgram(shaderProgram);
+  // start using shading program for rendering
+  gl.useProgram(shaderProgram);
 
-    // If creating the shader program failed, alert
-    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-        alert("Unable to initialize the shader program.");
-    }
+  // store location of aVertexPosition variable defined in shader
+  shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
 
-    // start using shading program for rendering
-    gl.useProgram(shaderProgram);
+  // turn on vertex position attribute at specified position
+  gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
 
-    // store location of aVertexPosition variable defined in shader
-    shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+  // store location of vertex normals variable defined in shader
+  shaderProgram.vertexNormalAttribute = gl.getAttribLocation(shaderProgram, "aVertexNormal");
 
-    // turn on vertex position attribute at specified position
-    gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+  // turn on vertex normals attribute at specified position
+  gl.enableVertexAttribArray(shaderProgram.vertexNormalAttribute);
 
-    // store location of vertex normals variable defined in shader
-    shaderProgram.vertexNormalAttribute = gl.getAttribLocation(shaderProgram, "aVertexNormal");
+  // store location of texture coordinate variable defined in shader
+  shaderProgram.textureCoordAttribute = gl.getAttribLocation(shaderProgram, "aTextureCoord");
 
-    // turn on vertex normals attribute at specified position
-    gl.enableVertexAttribArray(shaderProgram.vertexNormalAttribute);
+  // turn on texture coordinate attribute at specified position
+  gl.enableVertexAttribArray(shaderProgram.textureCoordAttribute);
 
-    // store location of texture coordinate variable defined in shader
-    shaderProgram.textureCoordAttribute = gl.getAttribLocation(shaderProgram, "aTextureCoord");
-
-    // turn on texture coordinate attribute at specified position
-    gl.enableVertexAttribArray(shaderProgram.textureCoordAttribute);
-
-    // store location of uPMatrix variable defined in shader - projection matrix
-    shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
-    // store location of uMVMatrix variable defined in shader - model-view matrix
-    shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
-    // store location of uNMatrix variable defined in shader - normal matrix
-    shaderProgram.nMatrixUniform = gl.getUniformLocation(shaderProgram, "uNMatrix");
-    // store location of uSampler variable defined in shader
-    shaderProgram.samplerUniform = gl.getUniformLocation(shaderProgram, "uSampler");
-    // store location of uMaterialShininess variable defined in shader
-    shaderProgram.materialShininessUniform = gl.getUniformLocation(shaderProgram, "uMaterialShininess");
-    // store location of uShowSpecularHighlights variable defined in shader
-    shaderProgram.showSpecularHighlightsUniform = gl.getUniformLocation(shaderProgram, "uShowSpecularHighlights");
-    // store location of uUseTextures variable defined in shader
-    shaderProgram.useTexturesUniform = gl.getUniformLocation(shaderProgram, "uUseTextures");
-    // store location of uUseLighting variable defined in shader
-    shaderProgram.useLightingUniform = gl.getUniformLocation(shaderProgram, "uUseLighting");
-    // store location of uAmbientColor variable defined in shader
-    shaderProgram.ambientColorUniform = gl.getUniformLocation(shaderProgram, "uAmbientColor");
-    // store location of uPointLightingLocation variable defined in shader
-    shaderProgram.pointLightingLocationUniform = gl.getUniformLocation(shaderProgram, "uPointLightingLocation");
-    // store location of uPointLightingSpecularColor variable defined in shader
-    shaderProgram.pointLightingSpecularColorUniform = gl.getUniformLocation(shaderProgram, "uPointLightingSpecularColor");
-    // store location of uPointLightingDiffuseColor variable defined in shader
-    shaderProgram.pointLightingDiffuseColorUniform = gl.getUniformLocation(shaderProgram, "uPointLightingDiffuseColor");
+  // store location of uPMatrix variable defined in shader - projection matrix 
+  shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
+  // store location of uMVMatrix variable defined in shader - model-view matrix 
+  shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+  // store location of uNMatrix variable defined in shader - normal matrix 
+  shaderProgram.nMatrixUniform = gl.getUniformLocation(shaderProgram, "uNMatrix");
+  // store location of uSampler variable defined in shader
+  shaderProgram.samplerUniform = gl.getUniformLocation(shaderProgram, "uSampler");
+  // store location of uMaterialShininess variable defined in shader
+  shaderProgram.materialShininessUniform = gl.getUniformLocation(shaderProgram, "uMaterialShininess");
+  // store location of uShowSpecularHighlights variable defined in shader
+  shaderProgram.showSpecularHighlightsUniform = gl.getUniformLocation(shaderProgram, "uShowSpecularHighlights");
+  // store location of uUseTextures variable defined in shader
+  shaderProgram.useTexturesUniform = gl.getUniformLocation(shaderProgram, "uUseTextures");
+  // store location of uUseLighting variable defined in shader
+  shaderProgram.useLightingUniform = gl.getUniformLocation(shaderProgram, "uUseLighting");
+  // store location of uAmbientColor variable defined in shader
+  shaderProgram.ambientColorUniform = gl.getUniformLocation(shaderProgram, "uAmbientColor");
+  // store location of uPointLightingLocation variable defined in shader
+  shaderProgram.pointLightingLocationUniform = gl.getUniformLocation(shaderProgram, "uPointLightingLocation");
+  // store location of uPointLightingSpecularColor variable defined in shader
+  shaderProgram.pointLightingSpecularColorUniform = gl.getUniformLocation(shaderProgram, "uPointLightingSpecularColor");
+  // store location of uPointLightingDiffuseColor variable defined in shader
+  shaderProgram.pointLightingDiffuseColorUniform = gl.getUniformLocation(shaderProgram, "uPointLightingDiffuseColor");
 }
 
 //
@@ -647,6 +647,7 @@ function drawScene() {
     gl.bindTexture(gl.TEXTURE_2D, cubeTexture);
     gl.uniform1i(shaderProgram.samplerUniform, 0);
 
+    gl.uniform1f(shaderProgram.materialShininessUniform, 1000);
     // Draw the cube.
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
     setMatrixUniforms();

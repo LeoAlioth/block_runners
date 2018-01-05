b700e4c4a5472338cec80581589e81daabea8c15
klopiiga <33962754+klopiiga@users.noreply.github.com>
2018-01-05 22:27:53 +0100

    Full Screen fix

--- scripts/main.js
+++ scripts/main.js
@@ -948,6 +948,8 @@ function initGL(canvas) {
     try {
         // Try to grab the standard context. If it fails, fallback to experimental.
         gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+        gl.canvas.width = window.innerWidth;
+        gl.canvas.height = window.innerHeight;
         gl.viewportWidth = canvas.width;
         gl.viewportHeight = canvas.height;
     } catch (e) {
